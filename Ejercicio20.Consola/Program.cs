dd34a96850c756798079af3a28186bcf171dfbd4
ciminoce <ciminoce@yahoo.com.ar>
2023-05-10 10:28:08 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio20.Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ingrese el primer número:");
+var numeroUno=int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número:");
+var numeroDos=int.Parse(Console.ReadLine());
+if (numeroUno==numeroDos)
+{
+    Console.WriteLine("Ambos números son iguales");
+}else if (numeroUno > numeroDos)
+{
+    Console.WriteLine($"{numeroUno} es mayor a {numeroDos}");
+}
+else
+{
+    Console.WriteLine($"{numeroDos} es mayor a {numeroUno}");
+
+}
